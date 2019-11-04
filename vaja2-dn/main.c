22d2775439aa9daa88d1a0d1856c75f71b70e6bd
Goran Kugler <goran.kugler@gmail.com>
2019-11-04 14:33:29 +0100

    Dodajam dn iz 2. vaj

--- vaja2-dn/main.c
+++ vaja2-dn/main.c
@@ -1,9 +1,36 @@
-Domača naloga
+/*Domača naloga
 1. Sestavi program, ki bo prebral tri realna števila in preveril, 
 ali obstaja trikontnik s takimi dolžinami stranic. Če obstaja, naj 
 program izračuna njegovo ploščino in obseg.
 
-2. Sestavi program, ki bo izračunal obseg in površino lika, 
-ki nastane dveh pravokotnikov.
+2. Sestavi program, ki bo izračunal obseg in površino dveh pravokotnikov.
 Njune stranice so vzporedne koordinatnima osema. Vsak pravokotnik je podan s 
-koordinatami dveh nasprotnih si oglišč.
+koordinatami dveh nasprotnih si oglišč.*/
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float a,b,c,s,p,o;
+    
+    printf("Vpisi tri realna stevila.\n");
+    printf("1.stevilo: "); scanf("%f",&a);
+    printf("2.stevilo: "); scanf("%f",&b);
+    printf("3.stevilo: "); scanf("%f",&c);
+    
+    if ((a==0)||(b==0)||(c==0)) printf("Stevila morajo biti razlicna od nic!\n");
+    else {
+        if((a<(b+c))&&(b<(a+c))&&(c<(a+b))) {
+            printf("Trikotnik s temi stranicami obstaja.\n");
+            o = (a+b+c);
+            s=0.5*o;
+            p=sqrt(s*(s-a)*(s-b)*(s-c));
+            printf("Obseg trikotnika je %g.\n",o);
+            printf("Ploscina trikotnika je %g.\n",p);
+        }
+        else printf("Trikotnik s temi stranicami ne obstaja.\n");
+    }
+    
+    return 0;
+}
