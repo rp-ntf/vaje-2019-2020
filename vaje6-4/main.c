29bcdeb4d0048702505ede391d0b752b45405f1e
Matjaz Bercic <matjaz.bercic@omm.ntf.uni-lj.si>
2019-11-19 10:21:00 +0100

    Med vajami

---
+++ vaje6-4/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Vaje : 
+ * Sestavite programe, ki :
+ * 1) od uporabnika prebere niz znakov in izpise dolzino niza znakov
+ * 2) od uprabnika prebere 2 niza znakov in pove kateri v imeniku pride prej
+ * 3) od uporabnika prebere 2 niza znakov in pove na katerem indeksu v 1. nizu se nahaja 2. niz
+ * 4) od uporabnika prebere 1 niz znakov in ga kot niz izpise v obratnem vrstenm redu
+ * 5) od uporabnika preberemo tabelo 5 celih stevil in jo uredimo po velikosti in izpisemo
+ * 
+ */
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
+}
