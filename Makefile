a9d7ed01a63106f18126eff23cf4f97b12bb81b2
Matjaz Bercic <matjaz.bercic@omm.ntf.uni-lj.si>
2019-12-02 15:16:16 +0100

    Dodajam resitev domace naloge

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ vaja7-4 - Debug ]----------"
-	@cd "vaja7-4" && "$(MAKE)" -f  "vaja7-4.mk"
+	@echo "----------Building project:[ vaja7-dn - Debug ]----------"
+	@cd "vaja7-dn" && "$(MAKE)" -f  "vaja7-dn.mk"
 clean:
-	@echo "----------Cleaning project:[ vaja7-4 - Debug ]----------"
-	@cd "vaja7-4" && "$(MAKE)" -f  "vaja7-4.mk" clean
+	@echo "----------Cleaning project:[ vaja7-dn - Debug ]----------"
+	@cd "vaja7-dn" && "$(MAKE)" -f  "vaja7-dn.mk" clean
--- vaja7-dn/main.c
+++ vaja7-dn/main.c
@@ -11,9 +11,68 @@
 
 
 #include <stdio.h>
+#include <math.h>
+
+int kvadratna_enacba( 
+	float a, float b, float c,
+	float koreni[2] )
+{
+	// Vrenmo korene v koreni[]
+	// vremo stevilo realnih nicel
+	float D = b*b - 4*a*c;
+	if( D < 0 )// Ni resitev
+	{
+		printf("Ni resitev kvadratne enacbe\n");
+		return 0;
+	}
+	else
+	{
+		koreni[0] = (-b-sqrt(D))/(2*a);
+		koreni[1] = (-b+sqrt(D))/(2*a);
+		printf("Resitvi kvadratne neacbe sta %f in %f\n", koreni[0], koreni[1] );
+		return 2;
+	}
+}
+
+int vsota_kvadratov( int n )
+{
+	int vsota = 0;
+	for( int i=0; i<=n; ++i )
+		vsota += i*i;
+	return vsota;
+}
+
+float varcevanje(
+	int st_let,
+	float obrok,
+	float obresti // v procentih
+	)
+{
+	float znesek = 0.0; // Na zacetku nimamo nic
+	for( int i=0; i<st_let; ++i)
+	{
+		znesek += obrok; // Na zacetku leta vplacamo obrok
+		znesek *= ( 1.0 + obresti / 100.0); // Obrestujemo do zdaj privarcevan znesek
+	}
+	return znesek;
+}
 
 int main(int argc, char **argv)
 {
-	printf("hello world\n");
+	float koreni[2];
+	kvadratna_enacba( 1, -5, 6, koreni );
+	
+	printf("Vsota kvadratov prvih 10 stevil je %d\n", vsota_kvadratov(10) );
+	
+	float obrok, obresti;
+	int st_let;
+	printf("Vnesite stevilo let varcevanja : ");
+	scanf("%d",&st_let);
+	printf("Vnesite obrok varcevanja : ");
+	scanf("%f", &obrok);
+	printf("Vnesite obresti varcevanja : ");
+	scanf("%f", &obresti);
+	printf("Po koncu varcevanja bo na racunu %f\n", 
+		varcevanje( st_let, obrok, obresti ) );
 	return 0;
 }
