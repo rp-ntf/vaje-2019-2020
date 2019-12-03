8bcf8e54ddc4b3f67d5adb130ea8fc495833b6e3
Matjaz Bercic <matjaz.bercic@omm.ntf.uni-lj.si>
2019-12-03 08:46:20 +0100

    Med vajami

--- vaja8-1/main.c
+++ vaja8-1/main.c
@@ -19,6 +19,25 @@ void izpisi_tabelo( int n, int tabela[] )
     // tabela[2] = 2*tabela[2];
 }
 
+/*
+ * Naloga: (funkcije tudi uporabite v programu)
+ * 1. Napisite funkcijo (izpisi_tabelo_float), 
+ * ki izpise tabelo realnih stevil,
+ * en element v vrstico (argumenta : dolzina tabele,
+ * in tabela)
+ * 2. Napisite funkcijo (dodaj_izvor), 
+ * ki sredinski element tabele poveca za 1
+ * 3. Napisite funkcijo (nastavi_robne_pogoje),
+ * ki zacetni in koncni element tabele nastavi na 0
+ * 4. Napisite funkcijo (povprecje),
+ * ki kot argumente prejme 2 tabeli enake dolzine
+ * in v drugi tabeli nastavi element i (i=1 do n-2)
+ * na povprecje elementov i-1, i in i+1 iz prve tabele
+ * 5. Napisite funkcijo (razlika tabel),
+ * ki izracuna povprecen kvdrat razlike clenov dveh tabel
+ * A in B tabeli: funkcija vrne vsoto (A[i]-B[i])^2
+ */
+
 int main(int argc, char **argv)
 {
     int vsota = 7;
