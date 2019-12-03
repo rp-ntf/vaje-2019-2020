a291e06e12078cfc8baf7f05425d32b16fbd2278
Matjaz Bercic <matjaz.bercic@omm.ntf.uni-lj.si>
2019-12-03 09:17:05 +0100

    Med vajami

--- vaja8-1/main.c
+++ vaja8-1/main.c
@@ -19,6 +19,39 @@ void izpisi_tabelo( int n, int tabela[] )
     // tabela[2] = 2*tabela[2];
 }
 
+void izpisi_tabelo_float( int n, float tabela[] )
+{
+    printf("\n");
+    for( int i=0; i<n; ++i )
+    {
+        printf("%f\t\n", tabela[i] );
+    }
+    printf("\n");
+}
+
+void dodaj_izvor( int n, float tabela[] )
+{
+    tabela[n/2] = tabela[n/2] + 1.0;
+    // tabela[n/2] += 1.0;
+}
+
+void nastavi_robne_pogoje( int n, float tabela[] )
+{
+    
+}
+
+void povprecje( int n, 
+    float tabela1[], float tabela2[] )
+{
+    
+}
+
+void razlika_tabel( int n,
+    float tabela1[], float tabela2[] )
+{
+    
+}
+
 /*
  * Naloga: (funkcije tudi uporabite v programu)
  * 1. Napisite funkcijo (izpisi_tabelo_float), 
@@ -26,7 +59,7 @@ void izpisi_tabelo( int n, int tabela[] )
  * en element v vrstico (argumenta : dolzina tabele,
  * in tabela)
  * 2. Napisite funkcijo (dodaj_izvor), 
- * ki sredinski element tabele poveca za 1
+ * ki sredinski element (tabela[n/2]) tabele poveca za 1
  * 3. Napisite funkcijo (nastavi_robne_pogoje),
  * ki zacetni in koncni element tabele nastavi na 0
  * 4. Napisite funkcijo (povprecje),
@@ -43,8 +76,11 @@ int main(int argc, char **argv)
     int vsota = 7;
     int tabela[5] = { 1, 2, 3, 4, 5 };
     int n = 5;
-    izpisi_tabelo(n,tabela);
-    izpisi_tabelo(n,tabela);
+    float tabela_float[9] = { 
+        1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    izpisi_tabelo_float(9,tabela_float);
+    dodaj_izvor(9,tabela_float);
+    izpisi_tabelo_float(9,tabela_float);
 
 	return 0;
 }
