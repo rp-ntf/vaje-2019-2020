e92edef1c7a7bea4a13b809453d38f8f526e82c8
Matjaz Bercic <matjaz.bercic@omm.ntf.uni-lj.si>
2020-09-14 14:54:29 +0200

    Update skripte za replit

--- zazeni-repl.sh
+++ zazeni-repl.sh
@@ -13,7 +13,16 @@ fi
 cd "$folder"
 
 echo "### Prevajam program..."
-gcc -o replrun *.c
+if [[ -e 'main.c' ]]
+then
+  gcc -o replrun *.c -lm
+elif [[ -e 'main.cpp' ]]
+then
+  g++ -o replrun *.cpp -lm
+else
+  echo "Ustrezna koda ne obstaja, koncujem"
+  exit 1
+fi
 echo "### Prevedeno"
 
 echo "### Zaganjam program"
